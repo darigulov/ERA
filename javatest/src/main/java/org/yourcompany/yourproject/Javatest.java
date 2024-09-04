48512bf47a5ab584ddf0bf5377d933726e0a4f94
darigulov <ernar4862@gmail.com>
2024-09-04 13:38:28 +0600

    первая лабка

---
+++ javatest/src/main/java/org/yourcompany/yourproject/Javatest.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author Admin
+ */
+public class Javatest {
+
+    public static void main(String[] args) {
+        System.out.println("Sd:" + addition(6, 9) + "S-d:" + subtraction(2, 4) + "S*d:" + multiplication(7, 2) +"S/d:" + division(2, 4));
+    }
+    public static int addition(int a,int b) {
+        return a + b;
+    }
+    public static int subtraction(int a,int b){
+        return a - b;
+    }
+    public static int multiplication(int a,int b){
+        return a * b;
+    }
+    public static int division(int a,int b){
+        return a / b;
+    }
+
+}
+
